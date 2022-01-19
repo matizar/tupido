8a557ce741963728f1386f1cce604de1536375e4
juancProfero <97607593+juancProfero@users.noreply.github.com>
2022-01-19 18:53:31 -0500

    Today Page - Widget
    Includes a widget to show calculated day  in the alternate calendar.

--- js/calendar.js
+++ js/calendar.js
@@ -1,22 +1,61 @@
 class Calendar{
+    lang = 'en';
+    strings ={};
     timeT = new TimeT();
-    solstice = 1608508800; /*21-12*2020 1608508800000*/
+    solstice = 346222800; /*1608508800; 21-12*2020 1608508800000*/
     seconds = 0;
     days = 0;
+    quad = 0;
+    figure = 0;
+    epoch = 0;
     years = 0;
+    dayOfYear = 0;
+    month = 0;
     evalDay(){
         this.seconds = this.timeT.now()-this.solstice;
+        /*this.seconds = 1608508800-this.solstice; */
         this.days = Math.floor(this.seconds/86400);
-        this.years = Math.floor(this.days/365.25);
+        this.quad = Math.floor(this.days / 1461);
+        this.figure = this.days-(1461 * (this.quad));
+        this.epoch = Math.floor(this.figure/365);
+        this.dayOfYear = this.figure-(this.epoch*365)
+        console.log(this.epoch);
+        this.years = (this.quad*4)+this.epoch;
+        /*this.years = Math.floor(this.days/1461);*/
+        this.month = Math.ceil(this.dayOfYear/28);
+        this.dayOfMonth = this.dayOfYear % 28 == 0 ? 28 : this.dayOfYear % 28;
     }
-    showCalendar(){
+    start(){
+        this.loadStrings('./js/strings.json', (text) => { 
+            this.strings = JSON.parse(text);
+            this.showCalendar();
+        });
+    }
+    loadStrings(file, callback) {
+        let rawFile = new XMLHttpRequest();
+        rawFile.overrideMimeType("application/json");
+        rawFile.open("GET", file, true);
+        rawFile.onreadystatechange = function () {
+            if (rawFile.readyState === 4 && rawFile.status == "200") {
+                callback(rawFile.responseText);
+            }
+        }
+        rawFile.send(null);
+    }
+    showCalendar(){   
         let app = document.getElementById('app');
         this.evalDay();
-        app.innerHTML = "Now: "+this.timeT.now()+"<br> Seconds:"+this.seconds+"<br> Days:"+this.days+"<br>Years: "+this.years;
+        console.info("Now: "+this.timeT.now()+" Seconds: "+this.seconds+" Days: "+this.days+" Years: "+this.years+" Quatter: "+this.quad+" Figure: "+this.figure+" Epoch: "+this.epoch+" Days of Year: "+this.dayOfYear);
+        let todayPage = document.createElement('div');
+        todayPage.setAttribute('id','today-page');
+        todayPage.setAttribute('class', 'today-page');
+        const months = this.strings[this.lang].months;
+        todayPage.innerHTML = "<span class='month-label'>" + months[this.month]+"</span><span class='today-number'>"+this.dayOfMonth+"</span><span class='year-number'>"+this.years+"</span>";
+        app.append(todayPage);
     }
 }
 window.onload = function(){
     console.info("window onload");
     let calendar = new Calendar;
-    calendar.showCalendar();
+    calendar.start();
 };
